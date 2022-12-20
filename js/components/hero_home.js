fbc7a310dec6e24d3ca89fb34fd060e54286bfa2
Lucas Mendez <lucasmendezbaca@gmail.com>
2022-12-20 08:29:22 +0100

    mover carpeta img a assets

--- js/components/hero_home.js
+++ js/components/hero_home.js
@@ -1,7 +1,7 @@
 export function heroHome() {
     const hero = document.createElement('div');
     hero.classList.add('hero');
-    hero.innerHTML = '<img src="img/hero.webp" alt="hero con ropa de navidad">';
+    hero.innerHTML = '<img src="assets/img/hero.webp" alt="hero con ropa de navidad">';
     
     return hero;
 }
--- js/components/siguenos.js
+++ js/components/siguenos.js
@@ -3,16 +3,16 @@ export function siguenos () {
     siguenos.classList.add('siguenos');
     siguenos.innerHTML = `
         <div class="siguenos__img_container">
-            <img class="siguenos__img" src="img/siguenos.webp" alt="imagen de modelos con ropa">
+            <img class="siguenos__img" src="assets/img/siguenos.webp" alt="imagen de modelos con ropa">
         </div>
         <div class="siguenos__info">
             <h3 class="siguenos__info__titulo">SÍGUENOS</h3>
             <p class="siguenos__info__parrafo">Te esperamos en nuestras redes sociales.<br>Aquí sucede todo. No te lo pierdas.</p>
             <div class="siguenos__info__redes">
-                <a href=""><i><img src="img/twitter.svg" alt="icono de twitter"></i></a>
-                <a href=""><i><img src="img/youtube.svg" alt="icono de youtube"></i></a>
-                <a href=""><i><img src="img/instagram.svg" alt="icono de instagram"></i></a>
-                <a href=""><i><img src="img/tiktok.svg" alt="icono de tiktok"></i></a>
+                <a href=""><i><img src="assets/img/twitter.svg" alt="icono de twitter"></i></a>
+                <a href=""><i><img src="assets/img/youtube.svg" alt="icono de youtube"></i></a>
+                <a href=""><i><img src="assets/img/instagram.svg" alt="icono de instagram"></i></a>
+                <a href=""><i><img src="assets/img/tiktok.svg" alt="icono de tiktok"></i></a>
             </div>
         </div>
     `;
